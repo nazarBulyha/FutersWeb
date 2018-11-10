1ab390f7774cf9c4082aeda87ba8d62045454ded
Nazar Bulyha <nazikkun97@gmail.com>
2018-11-10 20:29:05 +0200

    All works.

--- FuturesWeb/Scripts/Future/Index/Main.js
+++ FuturesWeb/Scripts/Future/Index/Main.js
@@ -20,23 +20,41 @@
 		$(table1).find(".row100.head ."+column).removeClass("hov-column-head-"+ verTable);
     });
 
-    $("#EnterCumulative").on("click", function () {
-        var val = $("#cumulativeText").val();
-        console.log(val);
-        //if (typeof(val) !== "undefined" && val !== null) {
+    // this is the id of the form
+    $("#idForm").submit(function(e) {
+        var form = $(this);
+        var url = form.attr("action");
+
         $.ajax({
-            url: "/Future/Index",
             type: "POST",
-            cache: false,
-            contentType: "application/html; charset=utf-8",
-            //dataType: "text",
+            url: url,
             async: true,
-            data: val,
-            success: function (result) {
-                $("#refTable").html(result);
+            data: form.serialize(), // serializes the form's elements.
+            success: function(data) {
+                $("#refTable").html(data);
             }
         });
 
-        return false;
+        e.preventDefault(); // avoid to execute the actual submit of the form.
     });
+
+    //$("#EnterCumulative").on("click", function () {
+    //    var val = $("#cumulativeText").val();
+    //    console.log(val);
+    //    //if (typeof(val) !== "undefined" && val !== null) {
+    //    $.ajax({
+    //        url: "/Future/Index",
+    //        type: "POST",
+    //        cache: false,
+    //        contentType: "application/html; charset=utf-8",
+    //        //dataType: "text",
+    //        async: true,
+    //        data: val,
+    //        success: function (result) {
+    //            $("#refTable").html(result);
+    //        }
+    //    });
+
+    //    return false;
+    //});
 })(jQuery);
